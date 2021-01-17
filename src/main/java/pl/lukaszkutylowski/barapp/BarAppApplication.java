e09077d3af0abdd3795740b43c25f947c4eba71e
lukasz-kutylowski <76553842+lukasz-kutylowski@users.noreply.github.com>
2021-01-17 10:47:41 +0100

    SpringBoot start class

---
+++ src/main/java/pl/lukaszkutylowski/barapp/BarAppApplication.java
@@ -0,0 +1,13 @@
+package pl.lukaszkutylowski.barapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarAppApplication.class, args);
+	}
+
+}
